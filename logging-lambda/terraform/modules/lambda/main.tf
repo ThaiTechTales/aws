f8a014ae095aff0aa75183d2e2602ec852edd56d
Thai Le <169008439+ThaiTechTales@users.noreply.github.com>
2025-01-08 17:29:05 +1100

    feat: complete logging-lambda project and update all readme files (#5)
    - Finalised the implementation of the "logging-lambda" project, including all required AWS Lambda functions and logging configurations.
    - Verified the deployment and functionality of the logging system.
    - Updated all README files to to provide accurate documentation for the repository and individual projects.

---
+++ logging-lambda/terraform/modules/lambda/main.tf
@@ -0,0 +1,8 @@
+resource "aws_lambda_function" "lambda_function" {
+  function_name    = var.function_name
+  role             = var.lambda_role_arn
+  handler          = var.handler
+  runtime          = var.runtime
+  s3_bucket        = var.s3_bucket
+  s3_key           = var.s3_key
+}
---
+++ logging-lambda/terraform/variables.tf
@@ -0,0 +1,55 @@
+# ---------------------------------
+# Provider: AWS
+# ---------------------------------
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+}
+
+# ---------------------------------
+# S3 bucket: Lambda code storage
+# ---------------------------------
+variable "bucket_name" {
+  description = "Name of the S3 bucket for Lambda code storage"
+  type        = string
+}
+
+# ---------------------------------
+# Lambda: function configuration
+# ---------------------------------
+
+variable "lambda_role_name" {
+  description = "Name of the IAM role for the Lambda function"
+  type        = string  
+}
+
+variable "lambda_policy_name" {
+  description = "Name of the IAM policy for the Lambda function"
+  type        = string  
+}
+
+variable "function_name" {
+  description = "Name of the Lambda function"
+  type        = string
+}
+
+variable "handler" {
+  description = "Lambda function handler (e.g., file.function_name)" # Entry point for the Lambda function
+  type        = string
+}
+
+variable "runtime" {
+  description = "Runtime environment for Lambda function (e.g., python3.9)"
+  type        = string
+}
+
+variable "s3_key" {
+  description = "S3 object key for the Lambda function code. Key refers to the path to the object in the S3 bucket"
+  type        = string
+}
+
+variable "retention_in_days" {
+  description = "Number of days to retain logs in CloudWatch Logs"
+  type        = number
+  default     = 7  
+}
